c15ba916d7e8d380e0d2c6e72db711ebf74f9848
matheus-hdas <matheussantos.betim@gmail.com>
2026-01-08 13:00:07 -0300

    build: add `libsql` adapter

--- api/src/libs/database.js
+++ api/src/libs/database.js
@@ -1,4 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
 
-const database = new PrismaClient();
+import "dotenv/config";
+
+const libsql = createClient({
+  url: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaLibSql(libsql);
+const database = new PrismaClient({ adapter });
 export default database;
