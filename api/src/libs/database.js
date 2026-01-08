684ae0cfaa5200639c9cef750eb68df04d76ec1c
matheus-hdas <matheussantos.betim@gmail.com>
2026-01-08 13:49:15 -0300

    fix: fix database with `better-sqlite3` adapter

--- api/src/libs/database.js
+++ api/src/libs/database.js
@@ -1,13 +1,11 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaLibSql } from "@prisma/adapter-libsql";
-import { createClient } from "@libsql/client";
-
 import "dotenv/config";
 
-const libsql = createClient({
+import { PrismaClient } from "@prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const adapter = new PrismaBetterSqlite3({
   url: process.env.DATABASE_URL,
 });
 
-const adapter = new PrismaLibSql(libsql);
 const database = new PrismaClient({ adapter });
 export default database;
